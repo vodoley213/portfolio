4756262ae3d9b14776d54586d2245a59827d6f23
vodoley <lampiduso@gmail.com>
2023-09-14 21:10:53 +0700

    Currency convertor done

--- JS/currency_app.js
+++ JS/currency_app.js
@@ -17,64 +17,73 @@ const options = {
   symbols: 'USD,EUR,RUB,TRY,KZT,CAD,GBP,CHF', // Швейцария - CHF
 }
 
-let currencyRatesToday
-let currencyRatesForYear
+let currencyRatesToday, currencyRatesForYear
+// let ratesTodayFromFetch, RatesForYearFromFetch
 
 // 1. Set default currencies and the base in options
-setCurrencies('USD', 'RUB')
+let currencyOne = 'USD'
+let currencyTwo = 'RUB'
+setCurrencies(currencyOne, currencyTwo)
 
-// 2. Загружаем данные с сервера и записываем их в массивы:
+// 2. Загружаем данные с сервера и записываем их в объекты:
 // currencyRatesToday и currencyRatesForYear
 
 currencyRatesToday = await getCurrencyToday(options)
+console.table(currencyRatesToday)
+
 currencyRatesForYear = await getCurrencyForYear(options)
+console.log(currencyRatesForYear)
+
+// ratesTodayFromFetch = { ...currencyRatesToday }
+// ratesTodayFromFetch.RUB = 100
+// console.log(currencyRatesToday)
+// console.log(ratesTodayFromFetch)
 
 // 3. Устанавливаем курсы валют в зивисимости от выбранных валют
 // setCurrencyRate()
 
 setCurrencyRate()
 
-inputField[0].addEventListener('input', () => {
-  inputField[1].value = formatNumber(
-    toNumber(inputField[0].value) * exchangeRate
-  )
-})
-
-inputField[1].addEventListener('input', () => {
-  inputField[0].value = formatNumber(
-    toNumber(inputField[1].value) / exchangeRate
-  )
-})
+inputField[0].addEventListener('input', setNumberInputField1)
+inputField[1].addEventListener('input', setNumberInputField0)
 
 button[0].addEventListener('click', () => {
   popupList[1].classList.add('hiding')
   popupList[0].classList.toggle('hiding')
   addOrRemoveOverlay()
 })
-
 button[1].addEventListener('click', e => {
   popupList[0].classList.add('hiding')
   popupList[1].classList.toggle('hiding')
   addOrRemoveOverlay()
 })
 
+// Закрытие оверлея
 document.addEventListener('click', e => {
   if (e.target.matches('[data-new-overlay]')) closeAllPopupsAndOverlay()
 })
 
-function addOrRemoveOverlay() {
-  if (popupList.every(list => list.classList.contains('hiding'))) {
-    header.removeAttribute('data-new-overlay')
-  } else {
-    header.setAttribute('data-new-overlay', true)
+// Смена валют
+popupList[0].addEventListener('click', e => {
+  const isClickedLi = e.target.closest('li')
+  if (isClickedLi) {
+    currencyOne = isClickedLi.dataset.currency
+    setCurrencies(currencyOne, currencyTwo)
+    setCurrencyRate()
+    setNumberInputField1()
   }
-}
+})
 
-function closeAllPopupsAndOverlay() {
-  popupList[0].classList.add('hiding')
-  popupList[1].classList.add('hiding')
-  header.removeAttribute('data-new-overlay')
-}
+popupList[1].addEventListener('click', e => {
+  const isClickedLi = e.target.closest('li')
+  if (isClickedLi) {
+    currencyTwo = isClickedLi.dataset.currency
+    console.log(currencyTwo)
+    setCurrencies(currencyOne, currencyTwo)
+    setCurrencyRate()
+    setNumberInputField0()
+  }
+})
 
 // ----- Additional functions -----
 
@@ -91,28 +100,39 @@ function setCurrencies(currencyOne, currencyTwo) {
 }
 
 function setCurrencyRate() {
-  const currencyTwo = currencyCode[1].dataset.currencyCode
-  exchangeRate = currencyRatesToday[currencyTwo]
+  // const currencyTwo = currencyCode[1].dataset.currencyCode
+  exchangeRate =
+    currencyRatesToday[currencyTwo] / currencyRatesToday[currencyOne]
+}
+
+function setNumberInputField1() {
+  inputField[1].value = formatNumber(
+    toNumber(inputField[0].value) * exchangeRate
+  )
+}
+
+function setNumberInputField0() {
+  inputField[0].value = formatNumber(
+    toNumber(inputField[1].value) / exchangeRate
+  )
 }
 
 // Получение курсов валют на сегодня
 async function getCurrencyToday(options) {
   gettinDataMessage.classList.remove('hiding')
 
-  // const url = `${URL}/latest?${params(options)}`
-
-  const url = 'JS/currency_latest.json'
+  const url = `${URL}/latest?${params(options)}`
 
   const currencyRates = await (await fetch(url).catch(handleError)).json()
 
   gettinDataMessage.classList.add('hiding')
   if (currencyRates.code === 400) {
-    console.log('Это ошибка с getCurrencyToday')
-
     errorMessage.classList.remove('hiding')
-    return
-  }
 
+    const localUrl = 'JS/currency_latest.json'
+    const savedCurrencyRates = await (await fetch(localUrl)).json()
+    return savedCurrencyRates.rates
+  }
   return currencyRates.rates
 }
 
@@ -121,22 +141,21 @@ async function getCurrencyForYear(options) {
   gettinDataMessage.classList.remove('hiding')
 
   const { startDate, endDate } = datesForDisplayCurrency()
-  // const url = `${URL}/timeseries?start_date=${startDate}&end_date=${endDate}${params(
-  //   options
-  // )}`
-  const url = 'JS/currency_timeseries.json'
+  const url = `${URL}/timeseries?start_date=${startDate}&end_date=${endDate}${params(
+    options
+  )}`
 
   const responseFromServer = await fetch(url).catch(handleError)
   const currencyRates = await responseFromServer.json()
 
   gettinDataMessage.classList.add('hiding')
   if (currencyRates.code === 400) {
-    console.log('Это ошибка с getCurrencyForYear')
     errorMessage.classList.remove('hiding')
-    return
-  }
 
-  // console.log(currencyRates.rates)
+    const localUrl = 'JS/currency_timeseries.json'
+    const savedCurrencyRates = await (await fetch(localUrl)).json()
+    return savedCurrencyRates
+  }
   return currencyRates.rates
 }
 
@@ -168,6 +187,20 @@ function handleError() {
     })
   )
   return resp
+}
+
+function addOrRemoveOverlay() {
+  if (popupList.every(list => list.classList.contains('hiding'))) {
+    header.removeAttribute('data-new-overlay')
+  } else {
+    header.setAttribute('data-new-overlay', true)
+  }
+}
+
+function closeAllPopupsAndOverlay() {
+  popupList[0].classList.add('hiding')
+  popupList[1].classList.add('hiding')
+  header.removeAttribute('data-new-overlay')
 }
 
 // const result = params(options)
